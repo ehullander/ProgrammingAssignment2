142b8e5b387bf8740ce517203d7bef549a2c3a4c
Eric Hullander <eric.hullander@gmail.com>
2015-05-19 19:36:44 -0700

    cachematrix
    Added code and comments for assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+##makeCacheMatrix creates a list of functions stored with values super assigned
+##accepts argument x which must be an invertible matrix 'x'
+##Let blob<-makeCacheMatrix(x)
+makeCacheMatrix <- function(x = numeric()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) inv <<- inverse
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
+##cacheSolve accepts the list of functions created by makeCacheMatrix  
+##it looks to see if the inverse has already been cached, if so it returns
+##the stored value, if not it calculates and pushes it into the list created by the first function
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setinv(inv)
+  inv
 }
